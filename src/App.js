b1f8186411fa23d23fffe1576072ff1a1fefade2
Jan Fasel <jan.fasel@volkswagen.de>
2021-07-19 15:14:38 +0200

    get hex color from img and display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import ColorThief from "colorthief";
 
 function App() {
   const [source, setSource] = React.useState("");
+
+  const [hexString, setHexString] = React.useState("");
 
   const handleCapture = target => {
     if (target.files) {
@@ -14,6 +17,29 @@ function App() {
     }
   };
 
+  const rgbToHex = ahh =>
+    "#" +
+    ahh
+      .map(x => {
+        return (+x).toString(16);
+      })
+      .join("");
+
+  React.useEffect(() => {
+    if (source && source !== "") {
+      const img = document.querySelector("img");
+      const colorThief = new ColorThief();
+
+      if (img.complete) {
+        setHexString(rgbToHex(colorThief.getColor(img)));
+      } else {
+        img.addEventListener("load", function() {
+          setHexString(rgbToHex(colorThief.getColor(img)));
+        });
+      }
+    }
+  }, [source]);
+
   return (
     <div className="container">
       <img src={source} className="snap-img" alt={"snap"}></img>
@@ -24,6 +50,7 @@ function App() {
         capture="environment"
         onChange={e => handleCapture(e.target)}
       />
+      <div>Dominant HexColor: {hexString}</div>
     </div>
   );
 }
